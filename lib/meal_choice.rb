aeab4d710b90a5ea818e8f53f034bb6d5d75280a
Julian Wakefield <julianwakefield@outlook.com>
2020-02-10 12:01:57 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,8 +2,8 @@
 # For output purposes, use "puts" instead of "print" or "p"
 def meal_choice(veg1="broccoli", veg2="macaroni", protein= "meat")
 
+puts "What a nutirious meal!"
 p "A plate of #{protein} with #{veg1} and #{veg2}."
 
 end
 
-meal_choice
