1e9099f04750f80fd97aedc73ce11a4cf0c73a20
Julian Wakefield <julianwakefield@outlook.com>
2020-02-10 11:55:10 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,6 +3,6 @@
 def meal_choice(veg1="broccoli", veg2="macaroni", protein= "meat")
 
 p "A plate of #{protein} with #{veg1} and #{veg2}."
-"What a nutritious meal!"
+puts "What a nutritious meal!"
 
 end
